776237080a5285523a0081acfe04b2e8ec395a18
phongdznhat <160645629+phongdznhat@users.noreply.github.com>
2024-02-21 00:47:08 +0700

    Add files via upload
    hi lô

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'tặng người yêu em';
+const contentCard = 'chúc người yêu em có một mùng 8/3 thật vui vẻ, hạnh phúc và không quên đỗ nguyện vọng 1 nữa nhé. Đừng quên yêu em nhiều hơn nữa nhé'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'em';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
